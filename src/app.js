a77d6420735fe652c236f216db379e9323a1f305
surajcodes017 <suraj.codes017@gmail.com>
2026-06-15 23:40:17 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
 const express = require('express');
 
 const app= express();
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server ne  brooo   !")
+// app.use("/user",(req,res)=>{
+//     res.send("HAHAHAHAHAH")
+// })
+
+
+let user = {
+     name: "Suraj",
+    age: 22
+}
+
+
+app.get("/user",(req,res)=>{
+    res.send(user)
+})
+app.put("/user",(req,res)=>{
+    res.send({
+        name:"virat kohli",
+        user
+    })
+})
+
+
+
+app.post("/user",(req,res)=>{
+    // save data to date base
+    res.send("Data successfully saved to database")
+})
+
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted data succesfully")
 })
 
 app.listen(7777,()=>{
-    console.log('server is listening on port  chalooo   7777......')
+    console.log('server is listening on port 7777......')
 })
