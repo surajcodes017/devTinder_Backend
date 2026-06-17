5733d59c00c5f66cd5822e635eeb5ae71c07a88b
surajcodes017 <suraj.codes017@gmail.com>
2026-06-18 02:32:06 +0530

    Exploring Mongoose Model Methods

--- src/app.js
+++ src/app.js
@@ -27,6 +27,108 @@ app.post("/signup",async(req,res) =>{
 })
 
 
+app.get("/user",async(req,res,next)=>{
+    // console.log(req.body);
+    // console.log(req.headers);
+    
+    
+    const userEmailId=req.body.emailId;
+
+
+
+    try{
+        const users = await User.findOne({"emailId": userEmailId});
+        console.log(users);
+        if(!users){
+            res.status(404).send("User not found");
+
+        }
+        if(users.length===0){
+            res.status(404).send("User not found");
+        }
+        else {
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+
+    }
+})
+app.get("/feed",async(req,res,next)=>{
+
+    try{
+        const users=await User.find({});
+        if(users.length===0){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+
+    }
+    catch(err){
+        res.status(400).send("something went wrong")
+    }
+})
+
+app.get("/feedone", async(req,res)=>{
+    try{
+        const users = await User.findById(req.body.id,"firstName lastName age")
+        if(!users){
+            res.status(404).send("User not dound by such id")
+        }
+        else{
+            res.send(users)
+        }
+    }
+    catch(err){
+            res.status(404).send("User not dound by such id")
+
+    }
+})
+
+app.delete("/user",async(req,res)=>{
+
+    try{
+        const deletedUser = await User.findByIdAndDelete(req.body.id);
+        if(!deletedUser){
+            res.status(404).send("User not found to do delete operation");
+        }
+        else{
+            res.send(deletedUser);
+        }
+    }
+     catch(err){
+            res.status(404).send("User not dound by such id")
+
+    }
+})
+
+
+app.patch("/user",async(req,res)=>{
+
+    try{
+        const users = await User.findByIdAndUpdate(
+            req.body.id,
+            {age:41},
+            {returnDocument:"after"}
+        
+        );
+        if(!users){
+            res.status(404).send("User not found by that Id");
+        }
+        else{
+            res.send(users);
+        }
+
+    }
+    catch(err){
+        res.status(400).send("something went wrong"+err.message);
+    }
+
+})
+
 
 connectDB()
     .then(() =>{
