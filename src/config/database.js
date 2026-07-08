cc66d99561e407c39d6ba56cc37ea488d2a90f11
surajcodes017 <suraj.codes017@gmail.com>
2026-07-08 15:12:23 +0530

    Updated DB password

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async() =>{
-    await mongoose.connect("mongodb+srv://NamasteyDev:4hQMr3luuNX23FKi@namasteynode.dxafcmu.mongodb.net/devTinder");
+    await mongoose.connect("mongodb+srv://NamasteyDev:KCDPHF1cA4clljN3@namasteynode.dxafcmu.mongodb.net/devTinder");
 }
 module.exports = {connectDB};
